c3b80bbac38e472ad25e579faabcbb71c31d4260
Atakan Senturk <blue.atakan.14@gmail.com>
2021-01-24 05:11:16 +0300

    last changes

--- ShoppingApp.PresentationLayer/Controllers/OrderController.cs
+++ ShoppingApp.PresentationLayer/Controllers/OrderController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShoppingApp.BusinessLogicLayer;
+using ShoppingApp.CoreLayer.EntitiesLayer;
 using ShoppingApp.DataAccessLayer.Concrete;
 using ShoppingApp.EntitiesLayer;
 using ShoppingApp.PresentationLayer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ShoppingApp.PresentationLayer.Controllers
@@ -24,6 +26,15 @@ namespace ShoppingApp.PresentationLayer.Controllers
         [HttpPost]
         public IActionResult AddOrder(CartToOrderVM vm)
         {
+            var userIdentity = (ClaimsIdentity)User.Identity;
+            Order order1 = new Order();
+            order1.ID = Guid.NewGuid();
+            order1.Status = Status.Active;
+            order1.OrderDescription = "Siparişlerim zamanında elime ulaşmıştır. Teşekkürler...";
+            order1.OrderDate = new DateTime(2021, 1, 18);
+            order1.UserID = new Guid(userIdentity.Name);
+            //order1.User = user1;
+            order1.OrderPrice = 9000;
             //_orderBLL.
             return RedirectToAction("Cart", "Cart");
         }
